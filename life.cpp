e43ada9e70c9b744cc872711d8fa9183377aec3d
Krishna Narayan <narayan@chapman.edu>
2021-09-18 11:27:35 -0700

    created life.cpp. implemented sequential GoL before parallelization

---
+++ life.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+class Cell{
+    Cell(){}
+    ~Cell(){}
+
+    // int animate(){}
+
+    // int kill(){}
+};
+
+class Grid{
+    Grid(){}
+    ~Grid(){}
+
+    void copy(){}
+};
+
+int main(int argc, char **argv){
+    cout << "hello world" << endl;
+    return 0;
+}
