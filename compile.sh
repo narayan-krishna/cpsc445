2b88dc08169e64df909c611f7134d359e96e4866
Krishna Narayan <narayan@chapman.edu>
2021-10-02 22:35:09 -0700

    testing branch for assign2

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
 g++ life.cpp -lpthread -o life.exe
 cat > input.txt <<EOF
-100
-100
-001
+0000000001
+0000010001
+0000101001
+0000000000
+0000000000
 EOF
-./life.exe input.txt output.txt 0 1
+./life.exe input.txt output.txt 19 30000
 cat output.txt
 rm output.txt
