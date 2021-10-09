ec8271ac550ea750f40682a1af95c706b59b224d
Krishna Narayan <narayan@chapman.edu>
2021-10-08 17:55:57 -0700

    changing back to original before break

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 g++ life.cpp -lpthread -o life.exe
 cat > input.txt <<EOF
-100
-100
-001
+0000000001
+0000010001
+0000101001
 EOF
-./life.exe input.txt output.txt 0 1
+./life.exe input.txt output.txt 10 5
 cat output.txt
 rm output.txt
