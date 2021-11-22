a18d907b5ae72e5914e2198c0cae458a9a7fa970
Krishna Narayan <>
2021-11-22 13:24:58 -0800

    cuda commit

--- dna_invert.cu
+++ dna_invert.cu
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+//blockId -> the block number
+//blockDim -> number of threads per block
+//threadIdx -> thread number within block
+
+//gid (1d) -> blockIdx.x * blockDim.x + threadIdx.x
+//gid (2d) -> blockIdx.x * blockDim.x * blockDim.y + threadIdx.y * blockDim.x * threadIdx.x
+
+//random change
+
 __global__ void reduce_sum(int * da, int N) {
-  int W = blockDim.x;
+    //array a [1,2,3,...,10,11,12]
+  int W = blockDim.x; 
   int tid = threadIdx.x;
   for(int i=tid+W; i<N; i+=W) da[tid]+=da[i];
   __syncthreads();
 
+//__shared__ int tmp[1024];
+//   tmp[gid] = da[gid];
 
-  for(int delta=1; delta<W; delta*=2) {
-    int i = tid*2*delta;
+
+  for(int delta=1; delta<W; delta*=2) { //set to 1, then set to 2 (w is 4 and
+    int i = tid*2*delta;                // delta doubles over loop
     if (i + delta < N) {
       da[i] += da[i+delta];
       printf("%i (%i): %i\n", i, delta, da[i]);
@@ -27,20 +40,20 @@ int main() {
 
   // set problem input (b)
   for (int i = 0; i<N; ++i) {
-    ha[i] = i*i;
+    ha[i] = i*i; //fill with i^2
   }
   
-  cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice); //copy ints from ha into da
 
-  int W = 16;
-  reduce_sum<<<1,W>>>(da, N);
+  int W = 16; //establish thread count
+  reduce_sum<<<1,W>>>(da, N); //call reduce sum using 1 block, 16 threads
   cudaDeviceSynchronize();
 
-  int sum;
-  cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost);
+  int sum; //sum in parallel
+  cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost); //copy back value of da int sum
 
   int expected_sum = (N-1)*N*(2*N-1)/6;
-  printf("%i (should be %i)", sum, expected_sum);
+  printf("%i (should be %i)", sum, expected_sum); //print sum
   cudaFree(da);
   free(ha);
   return 0;
