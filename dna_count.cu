85afabafd8c4b4c71ce71e6f9afb68110e17dff0
Krishna Narayan <narayan@chapman.edu>
2021-11-25 06:14:46 -0800

    quick commit to cuda

--- dna_count.cu
+++ dna_count.cu
@@ -6,35 +6,6 @@
 
 using namespace std;
 
-//blockId -> the block number
-//blockDim -> number of threads per block
-//threadIdx -> thread number within block
-
-//gid (1d) -> blockIdx.x * blockDim.x + threadIdx.x
-//gid (2d) -> blockIdx.x * blockDim.x * blockDim.y + threadIdx.y * blockDim.x * threadIdx.x
-
-//random change
-
-// __global__ void reduce_sum(int * da, int N) {
-//     //array a [1,2,3,...,10,11,12]
-//   int W = blockDim.x; 
-//   int tid = threadIdx.x;
-//   for(int i=tid+W; i<N; i+=W) da[tid]+=da[i];
-//   __syncthreads();
-
-// //__shared__ int tmp[1024];
-// //   tmp[gid] = da[gid];
-
-
-//   for(int delta=1; delta<W; delta*=2) { //set to 1, then set to 2 (w is 4 and
-//     int i = tid*2*delta;                // delta doubles over loop
-//     if (i + delta < N) {
-//       da[i] += da[i+delta];
-//       printf("%i (%i): %i\n", i, delta, da[i]);
-//     }
-//     __syncthreads();
-//   }
-// }
 void read_str(vector<int> &str, string file_name){
     ifstream input_stream (file_name);
     char c;
@@ -56,35 +27,21 @@ void read_str(vector<int> &str, string file_name){
     input_stream.close();
 }
 
-void print_sequence_file(const int *sequence, int length, string file_name) {
+void print_results_file(const int *char_counter, string file_name) {
+  char chars[4] = {'A', 'T', 'G', 'C'}; 
   ofstream out_file;
   out_file.open (file_name, fstream::app);
-
-  char chars[4] = {'A', 'T', 'G', 'C'}; 
-  for(int i = 0; i < length; i ++) {
-    out_file << chars[sequence[i]];
+  for(int i = 0; i < 4; i ++) {
+    out_file << chars[i] << " " << char_counter[i] << endl;
   }
-  out_file << endl;
   out_file.close();
 }
 
 //the sequence da, sequence length, n
-__global__ void invert(int *da, int N) {
+__global__ void count(int *da, int *dcounter, int N) {
   int tid = threadIdx.x;
-  printf("tid is: %i\n", tid);
-
-  int new_val; 
-  if (da[tid] == 0) {
-    new_val = 1;
-  } if (da[tid] == 1) {
-    new_val = 0;
-  } if (da[tid] == 2) {
-    new_val = 3;
-  } if (da[tid] == 3) {
-    new_val = 2;
-  }
-
-  da[tid] = new_val;
+  // printf("tid is: %i\n", tid);
+  dcounter[da[tid]] ++;
 }
 
 int main() {
@@ -98,8 +55,11 @@ int main() {
 
   cout << endl;
   int *ha = new int[N];
-  int *da;
+  int *hcounter = new int[4]{0};
+
+  int *da, *dcounter;
   cudaMalloc((void **)&da, N*sizeof(int));
+  cudaMalloc((void **)&dcounter, 4*sizeof(int));
 
   // set problem input (b)
   for (int i = 0; i<N; ++i) {
@@ -108,18 +68,19 @@ int main() {
   puts("\n");
   
   cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice); //copy ints from ha into da
+  cudaMemcpy(dcounter, hcounter, 4*sizeof(int), cudaMemcpyHostToDevice); //copy ints from ha into da
 
   // int W = 16; //establish thread count
   // reduce_sum<<<1,W>>>(da, N); //call reduce sum using 1 block, 16 threads
 
-  invert<<<1,N>>>(da, N);    
+  count<<<1,N>>>(da, dcounter, N);    
 
   cudaDeviceSynchronize();
 
   // int sum; //sum in parallel
-  cudaMemcpy(ha, da, N*sizeof(int), cudaMemcpyDeviceToHost); //copy back value of da int sum
+  cudaMemcpy(hcounter, dcounter, N*sizeof(int), cudaMemcpyDeviceToHost); //copy back value of da int sum
 
-  print_sequence_file(ha, N, "output.txt");
+  print_results_file(ha, N, "output.txt");
 
   // int expected_sum = (N-1)*N*(2*N-1)/6;
   // printf("%i (should be %i)", sum, expected_sum); //print sum
