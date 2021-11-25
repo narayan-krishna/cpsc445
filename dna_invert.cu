7e775a48df5a537acdefcef408f64d3e85ee418f
Krishna Narayan <narayan@chapman.edu>
2021-11-25 03:45:29 -0800

    quick commit to cuda

--- dna_invert.cu
+++ dna_invert.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 //blockId -> the block number
 //blockDim -> number of threads per block
@@ -9,30 +10,49 @@
 
 //random change
 
-__global__ void reduce_sum(int * da, int N) {
-    //array a [1,2,3,...,10,11,12]
-  int W = blockDim.x; 
+// __global__ void reduce_sum(int * da, int N) {
+//     //array a [1,2,3,...,10,11,12]
+//   int W = blockDim.x; 
+//   int tid = threadIdx.x;
+//   for(int i=tid+W; i<N; i+=W) da[tid]+=da[i];
+//   __syncthreads();
+
+// //__shared__ int tmp[1024];
+// //   tmp[gid] = da[gid];
+
+
+//   for(int delta=1; delta<W; delta*=2) { //set to 1, then set to 2 (w is 4 and
+//     int i = tid*2*delta;                // delta doubles over loop
+//     if (i + delta < N) {
+//       da[i] += da[i+delta];
+//       printf("%i (%i): %i\n", i, delta, da[i]);
+//     }
+//     __syncthreads();
+//   }
+// }
+
+//the sequence da, sequence length, n
+__global__ void invert(int *da, int N) {
   int tid = threadIdx.x;
-  for(int i=tid+W; i<N; i+=W) da[tid]+=da[i];
-  __syncthreads();
+  printf("tid is: %i", tid);
 
-//__shared__ int tmp[1024];
-//   tmp[gid] = da[gid];
-
-
-  for(int delta=1; delta<W; delta*=2) { //set to 1, then set to 2 (w is 4 and
-    int i = tid*2*delta;                // delta doubles over loop
-    if (i + delta < N) {
-      da[i] += da[i+delta];
-      printf("%i (%i): %i\n", i, delta, da[i]);
-    }
-    __syncthreads();
+  int new_val; 
+  if (da[tid] == 0) {
+    new_val = 1;
+  } if (da[tid] == 1) {
+    new_val = 0;
+  } if (da[tid] == 2) {
+    new_val = 3;
+  } if (da[tid] == 3) {
+    new_val = 2;
   }
+
+  da[tid] = new_val;
 }
 
 int main() {
   //INPUTS
-  int N = 1000;
+  int N = 8;
 
   int *ha = new int[N];
   int *da;
@@ -40,20 +60,30 @@ int main() {
 
   // set problem input (b)
   for (int i = 0; i<N; ++i) {
-    ha[i] = i*i; //fill with i^2
+    ha[i] = rand() % 4;
+    printf("%i", ha[i]);
   }
+  puts("\n");
   
   cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice); //copy ints from ha into da
 
-  int W = 16; //establish thread count
-  reduce_sum<<<1,W>>>(da, N); //call reduce sum using 1 block, 16 threads
+  // int W = 16; //establish thread count
+  // reduce_sum<<<1,W>>>(da, N); //call reduce sum using 1 block, 16 threads
+
+  invert<<<1,1>>>(da, N);    
+
   cudaDeviceSynchronize();
 
-  int sum; //sum in parallel
-  cudaMemcpy(&sum, da, sizeof(int), cudaMemcpyDeviceToHost); //copy back value of da int sum
+  // int sum; //sum in parallel
+  cudaMemcpy(ha, da, N*sizeof(int), cudaMemcpyDeviceToHost); //copy back value of da int sum
 
-  int expected_sum = (N-1)*N*(2*N-1)/6;
-  printf("%i (should be %i)", sum, expected_sum); //print sum
+  for (int i = 0; i<N; ++i) {
+    printf("%i", ha[i]);
+  }
+  puts("\n");
+
+  // int expected_sum = (N-1)*N*(2*N-1)/6;
+  // printf("%i (should be %i)", sum, expected_sum); //print sum
   cudaFree(da);
   free(ha);
   return 0;
